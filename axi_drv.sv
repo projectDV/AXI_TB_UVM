c6e9c20fca2b00580c3bf08f019cc73865d1343d
Siddhartha Gupta <siddhartha.gupta119@gmail.com>
2024-10-08 16:50:56 +0530

    Update axi_drv.sv
    Updated with more better code

--- axi_drv.sv
+++ axi_drv.sv
@@ -1,89 +1,140 @@
-class axi_drv extends uvm_driver#(axi_seq_item);
-	`uvm_component_utils(axi_drv)
-	virtual axi_if.mdrv vif;                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                     vif;
-	axi_seq_item w_txn;
-	axi_seq_item r_txn;//not yet made logic for it. Only did for write txn
-	axi_cfg cfg;
-	logic awvalid, wvalid;
-	function new(name=axi_drv, uvm_compnent parent)
-			super.new(name,parent);
-			vif=new();
-			w_txn=new();
-			r_txn=new();
-	endfunction
-	
-	function void build_phase(uvn_phase build_phase);
-		super.build_phase(phase);
-		if(!uvm_config_db#(virtual axi_if.mdrv )::get(this, "","vif",cfg))
-			`uvm_error(get_type_name(),"Not set at top level")//get_type_name shows the class name to which this is associated.
-			//`uvm_error(get_full_name(),"Not set at top level")[get_full_name is also used to print the whole hierarchy.]
-	endfunction
-	
-	function void connect_phase(uvm_phase phase)'
-		super.connect_phase(phase);
-		this.vif=cfg.vif
-	endfunction 
-	
-	task run_phase(uvm_phase phase);
-		super.run_phase(phase);
-		`uvm_info(get_type_name(),"Run_Phase is starting", UVM_LOW)
-		vsif.m_drv_cb.bready<=1;
-		forever begin
-			drive();
-		end
-	endtask
-	
-	task drive();
-		if(!vif.rstn)begin//reset is low [Active State]
-			vsif.m_drv_cb.awvalid<=0;
-			vsif.m_drv_cb.wvalid<=0;
-			vsif.m_drv_cb.arvalid<=0;
-		end
-		else begin
-			fork begin
-				seq_item_port.get_next_item(w_txn);
-				`uvm_info(get_name(),"Packet Received In DRV",UVM_LOW)
-				@(vsif.m_drv_cb);
-					vsif.m_drv_cb.awaddr<=w_txn.awaddr;//driving addr
-					vsif.m_drv_cb.awlen<=w_txn.awlen;
-					vsif.m_drv_cb.awsize<=w_txn.awsize;
-					vsif.m_drv_cb.awbusrt<=w_txn.awbusrt;
-					//assert awvalid->wait 1 cycle-> de-assert awvalid
-					@(vsif.m_drv_cb);
-					awvalid=1;
-					vsif.m_drv_cb.awvalid<=awvalid;
-					
-					wait(vsif.m_drv_cb.awready);
-					awvalid=0;
-					vsif.m_drv_cb.awvalid<=awvalid;
-					
-					wait(vsif.m_drv_cb.bvalid);
-					
-					vsif.m_drv_cb.wdata<=w_txn.wdata;// Driving data
-					`uvm_info("DRV", "Data Driven", UVM_HIGH)
-					//assert wvalid->wait 1 cycle-> de-assert wvalid->wait for bvalid
-					@(vsif.m_drv_cb);
-						wvalid=1;
-						vsif.m_drv_cb.wvalid<=wvalid;
-						
-						wait(vsif.m_drv_cb.wready)
-						wvalid=0;
-						vsif.m_drv_cb.wvalid=wvalid;
-						
-						wait(vsif.m_drv_cb.bvalid)
-				seq_item_port.item_done();
-				end
-			join_none
-		end
-	endtask
-endclass  
-	
-					
-				
-					
-				
-					
-					
-			
-			
-			
+class axi_driver extends uvm_driver#(txn);
+    `uvm_component_utils(axi_driver)
+
+    //vif
+    virtual intf vif;
+    //txn
+    seq_item txn;
+    //cfg
+    tb_cfg cfg;
+    //tlm
+    uvm_tlm_port#(txn)gen2drv;
+    extern drive();
+    extern wr_addr();
+    extern wr_data();
+    extern wr_resp();
+    extern rd_addr();
+    extern rd_data_resp();
+
+    function new(string name="axi_driver", uvm_component parent);
+        super.new(name,parent);
+    endfunction
+
+    function build_phase(uvm_phase phase);
+        super.build_phase(phase);
+        if(!(uvm_config_db(tb_cfg cfg)::get(this, "", "vif", cfg.vif)))
+            `uvm_error(get_type_name(), "Not Set Properly At Top");
+    endfunction
+
+    function connect_phase(uvm_phase phase);
+        super.connect_phase(phase);
+        this.vif=cfg.vif;
+    endfunction
+
+    task run_phase(uvm_phase phase);
+        forever begin
+            @(posedge vif.aclk)
+            //vif.bready  <=  1; both are used individually inside wr_resp() and rd_resp()
+            //vif.rready  <=  1;
+            vif.awready   <=  1;
+            vif.wready    <=  1;
+            vif.arready   <=  1;
+            vif.rvalid    <=  1;
+            vif.bvalid    <=  1;
+
+            sequence_item_port.get_next_item(txn);
+                drive();
+            sequence_item_port.item_done(txn);
+
+        end
+    endtask
+endclass
+    
+    task axi_driver::drive();//1. write addr phase 2. write data phase 3. wr_resp phase 4. rd_addr phase 5. rd_data/resp phase
+        fork
+        if(!vif.arstn)
+            vif.awvalid<=1 ;
+            wr_addr();
+            @(vif.aclk);
+            wait(vif.awready);
+            #100;
+            vif.awvalid<=0;
+            @(vif.aclk);
+            vif.wvalid<=1;
+            wr_data();
+            @(vif.aclk);
+            wr_resp();
+            @(vif.aclk);
+            vif.bready<=0;
+            #100;
+            @(vif.aclk);
+            rd_addr();
+        join_none
+    endtask
+    task axi_driver::wr_addr();
+        fork
+            @(vif.aclk);
+                vif.awid<=txn.awid;
+                vif.awaddr<=txn.awaddr;
+                vif.awlen<=txn.awlen;
+                vif.awsize<=txn.awsize;
+                vif.awburst<=txn.awburst;
+        join_none
+    endtask
+    task axi_driver::wr_data();
+       
+        fork
+            @(vif.aclk);
+            if((vif.awvalid || vif.wvalid) && (vif.awready || vif.wready) )
+                int len;
+                len=(txn.wlen+1);
+                for(int i=0; i<len; i++)
+                    vif.wid<=   txn.wid;
+                    vif.wdata<= txn.wdata[i];
+                    vif.strb<=  txn.wstrb;
+                    vif.wlast<= (i=len-1)?1:0;
+                    //wait(vif.wready)
+                    @(vif.aclk);
+                    vif.wvalid<=0;
+        join_none
+    endtask
+    task axi_driver::wr_resp();
+      fork
+        @(vif.aclk)
+        wait(vif.bvalid)
+        txn.bid<=vif.bid;
+        txn.bresp<=vif.bresp;
+        vif.bready<=1; 
+      join_none
+    endtask
+    task axi_driver::rd_addr();
+      fork
+        @(vif.aclk);
+        vif.arid<=txn.arid;
+        vif.araddr<=txn.araddr;
+        vif.arlen<=txn.arlen;
+        vif.arbust<=txn.arburst;
+        vif.arsize<=txn.arsize;
+
+        vif.arvalid<=1;
+        wait(vif.arready);
+        @(vif.aclk);
+        vif.valid<=0;
+      join_none
+    endtask
+    task axi_driver::rd_data_resp();
+      fork
+        int len=vif.arlen+1;
+        for(int i=0; i<len; i++)begin
+          wait(vif.rvalid);
+          txn.rid<=vif.rid;
+          txn.rdata[i]<=vif.rdata;
+          txn.rresp<=vif.rresp;
+          if(vif.rlast)
+            break;
+          vif.rready<=1;
+          @(vif.aclk);
+          vif.rready<=0;
+        end
+    join_none
+    endtask
