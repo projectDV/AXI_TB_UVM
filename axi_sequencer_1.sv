f2d69d844fe69fe62e1b0fce501837e5bf71e17d
Siddhartha Gupta <siddhartha.gupta119@gmail.com>
2024-10-18 16:36:26 +0530

    Update and rename axi_seqr.sv to axi_sequencer_1.sv

---
+++ axi_sequencer_1.sv
@@ -0,0 +1,10 @@
+class axi_seqeuncer_1 extends uvm_sequencer;
+    `uvm_component_utils(axi_seqeuncer_1)
+
+    function new(string name="axi_sequencer_1", uvm_component parent);
+        super.new(name, parent);
+    endfunction
+    function build_phase(uvm_phase phase);
+        super.build_phase(phase);
+    endfunction
+endclass
